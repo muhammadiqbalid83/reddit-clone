6b26211114fdc0fcb73ef2de96131c490dd4b2fd
Muhammad Iqbal <muhammadiqbalid83@gmail.com>
2023-02-21 17:24:55 +0700

    Chakra setup

--- src/chakra/theme.ts
+++ src/chakra/theme.ts
@@ -1,12 +1,26 @@
 // 1. Import the extendTheme function
+import "@fontsource/open-sans/300.css";
+import "@fontsource/open-sans/400.css";
+import "@fontsource/open-sans/700.css";
 import { extendTheme } from "@chakra-ui/react";
 
 // 2. Extend the theme to include custom colors, fonts, etc
 const colors = {
   brand: {
-    900: "#1a365d",
-    800: "#153e75",
-    700: "#2a69ac",
+    900: "#ff3c00",
+  },
+  fonts: {
+    body: "Open Sans, sans-serif",
+  },
+  styles: {
+    global: () => ({
+      body: {
+        bg: "gray.200",
+      },
+    }),
+  },
+  components: {
+    //Button
   },
 };
 
